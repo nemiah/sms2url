c9d5f7f9b737eabec8bbc651f850118a8d8b38f2
Nena Furtmeier <Nena@Furtmeier.IT>
2017-10-12 22:29:01 +0200

    added button in settings to set as default app

--- app/src/main/java/it/furtmeier/sms2url/ActivitySettings.java
+++ app/src/main/java/it/furtmeier/sms2url/ActivitySettings.java
@@ -1,25 +1,20 @@
 package it.furtmeier.sms2url;
 
 import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.preference.ListPreference;
 import android.preference.Preference;
-import android.preference.PreferenceActivity;
+import android.preference.PreferenceScreen;
+import android.provider.Telephony;
 import android.support.v7.app.ActionBar;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
-import android.preference.RingtonePreference;
-import android.text.TextUtils;
 import android.view.MenuItem;
 
-import java.util.List;
 
 public class ActivitySettings extends AppCompatPreferenceActivity {
 
@@ -113,6 +108,17 @@ public class ActivitySettings extends AppCompatPreferenceActivity {
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     public static class GeneralPreferenceFragment extends PreferenceFragment {
         @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+
+            if(Telephony.Sms.getDefaultSmsPackage(getActivity()).equals(getActivity().getPackageName())) {
+                PreferenceScreen screen = getPreferenceScreen();
+                Preference pref = getPreferenceManager().findPreference("setDefault");
+                screen.removePreference(pref);
+            }
+        }
+
+        @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.pref_general);
@@ -120,6 +126,26 @@ public class ActivitySettings extends AppCompatPreferenceActivity {
 
             bindPreferenceSummaryToValue(findPreference("phone"));
             bindPreferenceSummaryToValue(findPreference("url"));
+
+            Preference button = findPreference("setDefault");
+            if(Telephony.Sms.getDefaultSmsPackage(getActivity()).equals(getActivity().getPackageName())) {
+                PreferenceScreen screen = getPreferenceScreen();
+                Preference pref = getPreferenceManager().findPreference("setDefault");
+                screen.removePreference(pref);
+            }
+
+            button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+
+                    Intent intent = new Intent(Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT);
+                    final String packageName = getActivity().getPackageName();
+                    intent.putExtra(Telephony.Sms.Intents.EXTRA_PACKAGE_NAME, packageName);
+                    startActivityForResult(intent, 0);
+
+                    return true;
+                }
+            });
         }
 
         @Override
